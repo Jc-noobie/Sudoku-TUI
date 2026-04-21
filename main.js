c4579bf1b1a0ed6d45e86b014c1b37129c632fe4
Joed <joeddonaire@gmail.com>
2026-04-22 02:13:23 +0800

    Started fresh

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env/ node
+
 import inquirer from "inquirer";
 import chalk from 'chalk';
 import chalkAnim from 'chalk-animation' ;
@@ -6,3 +8,4 @@ import { createSpinner } from "nanospinner";
 import gradient from "gradient-string";
 import boxen from 'boxen' ;
 
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r,ms)) ;
